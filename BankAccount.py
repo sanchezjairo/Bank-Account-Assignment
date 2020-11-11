cdf78f2347c3505bdef07a9fd48e0151b36debd5
Jairo Sanchez <jairosanchez@Jairos-MacBook-Air.local>
2020-11-10 18:09:43 -0800

    changed account_number attribute

--- BankAccount.py
+++ BankAccount.py
@@ -6,7 +6,7 @@ import random
 class BankAccount:
     def __init__(self, full_name, account_number, routing_number, balance):
         self.full_name = full_name,
-        self.account_number = random.randint(100000000,999999999)
+        self.account_number = str(random.randint(100000000,999999999))
         self.routing_number = 475935201
         self.balance = 0
     def deposit(self,amount):
@@ -17,7 +17,10 @@ class BankAccount:
         if amount > self.balance:
             print('Insufficient funds.')
             self.balance = self.balance - amount - 10
+            amount = "{:.2f}".format(self.balance)
         else:
+            self.balance = self.balance-amount
+            amount = "{:.2f}".format(self.balance)
             print(f'Amount Withdrawn:{amount}')
     def get_balance(self):
         print(f'Hello good day your balance is {self.balance}')
@@ -26,7 +29,5 @@ class BankAccount:
         interest = self.balance * 0.00083
         self.balance = self.balance + interest
     def recipt(self):
-        print(f'\n{self.full_name}\n Account No.:{self.account_number}\n Routing No.:{self.routing_number}\n Balance.:{self.balance}\n')
-jairo = BankAccount('mike sanchez' ,0,0,0)
-jairo.deposit(200)
-jairo.recipt()
+        self.account_number = (self.account_number[0:4])
+        print(f'\n{self.full_name}\n Account No.:****{self.account_number}\n Routing No.:{self.routing_number}\n Balance.:{"{:.2f}".format(self.balance)}\n')
